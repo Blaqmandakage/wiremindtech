1e7f766a20f227995bab9526e893ee6e04c2a403
blaqmandakage <oluwatosinadara0@gmail.com>
2026-04-22 11:05:03 -0700

    new changes

--- scripts/utils/darkmode.js
+++ scripts/utils/darkmode.js
@@ -11,10 +11,10 @@ toggleBtn.addEventListener("click", () => {
 
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
-    toggleBtn.textContent = "☀️ Light Mode";
+   
   } else {
     localStorage.setItem("theme", "light");
-    toggleBtn.textContent = "🌙 Dark Mode";
+  
   }
 });
 }
